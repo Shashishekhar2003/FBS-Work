6e60d89f6d1679d1e1fb8546a000340d2b0c2ce0
Shashishekhar <shashibache2003@gmail.com>
2025-12-04 14:29:32 +0530

    Add files via upload

---
+++ C Module/Assignments/Assignment 04/A5.c
@@ -0,0 +1,53 @@
+/* 5. Write a menu driven program to take a number for user and perform operations as follows.
+
+Press 1.To check number is even or odd.
+2.To check number is prime or not.
+3.To check number is pallindrome or not.
+4.To check number is positive, negative or zero.
+5.To reverse a number.
+6.To find sum of digits. */
+
+#include<stdio.h>
+void main()
+{
+
+    int no;
+    printf("Enter thhe no :");
+    scanf(" %d ",&no);
+
+    printf("Enter 1 == Prime \n");
+    printf("Enter 2 == strong \n");
+    printf("Enter 3 == Amstrong\n");
+    printf("Enter 4 == perfect\n");
+    printf("Enter 5 == palindrome\n");
+
+    int choice;
+    print("enter the choice :");
+    scanf(" %d ",&choice);
+
+    if(choice==1)
+    {
+
+    }
+    else if(choice==2)
+    {
+
+    }
+    else if (choice==3)
+    {
+
+    }
+    else if(choice==4)
+    {
+        
+    }
+    else if(choice==4)
+    {
+        
+    }
+    else
+    {
+        printf("Choice is Invalid ");
+    }
+
+}
