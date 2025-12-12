a534f6a26dca982c1ae424c0c3fc1b654fa0d53a
Shashishekhar <shashibache2003@gmail.com>
2025-12-12 12:14:03 +0530

    Add files via upload

---
+++ C Module/Class Test/Test 6/Q2.c
@@ -0,0 +1,18 @@
+//Q2. WAP to find two Max number in array
+#include<stdio.h>
+void main()
+{
+    int arr1[5];
+
+    printf("Enter Array 1 : ");
+    for(int i=0 ; i<5 ; i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
+
+    for(int i=0 ; i<5 ; i++)
+    {
+        printf("%d ",arr1[i]);
+    }
+    
+}
