6221754b4aee86491e3036e9cdaec63ed80f6cb4
Shashishekhar <shashibache2003@gmail.com>
2025-12-16 12:24:20 +0530

    Add files via upload

---
+++ C Module/Classwork/14-May/arr.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    int arr[10];
+    printf("Enter the Elements :\n");
+    for(int i=0 ; i<10 ; i++)
+        scanf("%d",&arr[i]);
+
+    printf("Your Array is ");
+    for(int i=0 ; i<10 ; i++)
+        printf("%d ",arr[i]);
+
+    int max=arr[0];
+    for(int i=1 ; i<8 ; i++)
+        if(arr[i]>max)
+        max=arr[i];
+
+    printf("\n Max Element is : %d",max);
+
+    int min=arr[0];
+    for(int i=1 ; i<8 ; i++)
+        if(arr[i]<min)
+        min=arr[i];
+        
+
+    printf("\n Min Element is : %d",min);
+
+}
